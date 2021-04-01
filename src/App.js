2c5fcdbdcd5c6b5627b004095e6fb3a37d167865
Marcio <oliveira.c.marcio@gmail.com>
2021-03-31 22:02:56 -0300

    Added features to App, Emitter, Notifyer and Timer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { Notifyer } from './Notifyer.js';
 import { Timer } from './Timer.js';
+import { Emitter } from './Emitter.js';
+
+const notify = Notifyer.notify({ 
+  title: 'Time to post', 
+  body: 'Create engagement to build your community'});
 
 const App = {
   async start() {
     try {
-      const time = 25 * 60; 
-      //await Notifyer.init();
-      //Notifyer.notify({ title: 'Time to post', body: 'Create engagement to build your community'});
-      //
-      Timer.init();
+
+      await Notifyer.init();
+
+      Emitter.on('countdown-start', notify);
+
+      Emitter.on('countdown-end', Timer.init)
+      
+      Timer.init(0.1 * 600);
     } 
     catch (error) {
       console.log(error.message);
